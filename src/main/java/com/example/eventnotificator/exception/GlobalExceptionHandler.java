b5b4e2d68bb6e1d74780bff44cb2dabba2cf2a9f
NGaidarenko <nicita.gaidarenko@yandex.ru>
2024-08-16 16:02:44 +0300

    Add GlobalExceptionHandler

--- src/main/java/com/example/eventnotificator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/eventnotificator/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.eventnotificator.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,12 +12,34 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception e) {
+    public ResponseEntity<ErrorMessageResponse> handleException(Exception e) {
         ErrorMessageResponse exceptionMessageResponse = new ErrorMessageResponse(
                 "Internal error",
                 e.getMessage(),
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(exceptionMessageResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessageResponse> entityNotFoundedException(EntityNotFoundException e) {
+        ErrorMessageResponse errorMessageResponse = new ErrorMessageResponse(
+                "Not founded",
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorMessageResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorMessageResponse> illegalArgumentExceptionResponseEntity(IllegalArgumentException e) {
+        ErrorMessageResponse errorMessageResponse = new ErrorMessageResponse(
+                "Bad request",
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorMessageResponse, HttpStatus.BAD_REQUEST);
     }
 }
